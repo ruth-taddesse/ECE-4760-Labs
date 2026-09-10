7caf95ce3c32a40af6af45c699b4ecb1cf597fba
kab472 <kab472@cornell.edu>
2026-09-09 21:43:39 -0400

    added basic keyboard functionality

--- Lab_1/Lab_1_code/ADC_DDS_keypad.c
+++ Lab_1/Lab_1_code/ADC_DDS_keypad.c
@@ -10,12 +10,18 @@ Schedules a single thread, reads/prints ADC value
 #include "hardware/timer.h"
 #include "hardware/adc.h"
 #include "pico/stdlib.h"
+#include "pico/multicore.h"
 #include <stdio.h>
 #include <string.h>
 #include "stdlib.h"
 #include "hardware/spi.h"
 #include <math.h>
 #include "hardware/irq.h"
+#include "hardware/pio.h"
+#include "hardware/dma.h"
+#include "hardware/sync.h"
+#include "hardware/spi.h"
+#include "hardware/clocks.h"
 
 // ==========================================
 // === protothreads globals
@@ -35,6 +41,11 @@ Schedules a single thread, reads/prints ADC value
 #define two32 4294967296.0 // 2^32 
 #define Fs 50000
 #define DELAY 20 // 1/Fs (in microseconds)
+
+//keypad constants
+#define BASE_KEYPAD_PIN 9
+#define KEYROWS         4
+#define NUMKEYS         12
 // the DDS units:
 volatile unsigned int phase_accum_main;
 volatile unsigned int phase_incr_main;
@@ -68,6 +79,15 @@ static int map(int x, int in_min, int in_max, int out_min, int out_max) {
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
+//keycodes
+unsigned int keycodes[NUMKEYS] = {      0x57, 0x6E, 0x5E, 0x3E, 0x6D,
+                                        0x5D, 0x3D, 0x6B, 0x5B, 0x3B,
+                                        0x67, 0x37} ;
+unsigned int scancodes[KEYROWS] = {   0xE, 0xD, 0xB, 0x7} ;
+unsigned int button = 0x70 ;
+char keytext[40];
+int prev_key = 0;
+
 // ==================================================
 // === toggle25 thread 
 // ==================================================
@@ -94,6 +114,53 @@ static PT_THREAD (protothread_toggle25(struct pt *pt))
       // every thread ends with PT_END(pt);
       PT_END(pt);
 } // end blink thread
+
+// This thread runs on core 0
+static PT_THREAD (protothread_core_0(struct pt *pt))
+{
+    // Indicate thread beginning
+    PT_BEGIN(pt) ;
+
+    // Some variables
+    static int i ;
+    static uint32_t keypad ;
+
+    while(1) {
+
+        gpio_put(LED, !gpio_get(LED)) ;
+
+        // Scan the keypad!
+        for (i=0; i<KEYROWS; i++) {
+            // Set a row high
+            gpio_put_masked((0xF << BASE_KEYPAD_PIN),
+                            (scancodes[i] << BASE_KEYPAD_PIN)) ;
+            // Small delay required
+            sleep_us(1) ;
+            // Read the keycode
+            keypad = ((gpio_get_all() >> BASE_KEYPAD_PIN) & 0x7F) ;
+            // Break if button(s) are pressed
+            if ((~keypad) & button) break ;
+        }
+        // If we found a button . . .
+        if ((~keypad) & button) {
+            // Look for a valid keycode.
+            for (i=0; i<NUMKEYS; i++) {
+                if (keypad == keycodes[i]) break ;
+            }
+            // If we don't find one, report invalid keycode
+            if (i==NUMKEYS) (i = -1) ;
+        }
+        // Otherwise, indicate invalid/non-pressed buttons
+        else (i=-1) ;
+
+        // Print key to terminal
+        printf("\n%d", i) ;
+
+        PT_YIELD_usec(30000) ;
+    }
+    // Indicate thread end
+    PT_END(pt) ;
+}
 
 // Alarm ISR
 static void alarm_irq(void) {
@@ -135,45 +202,60 @@ int main(){
   adc_select_input(ADC_MUX) ;
 
   // Initialize stdio
-    stdio_init_all();
-    printf("Hello, DAC!\n");
+  stdio_init_all();
+  printf("Hello, DAC!\n");
 
-    // Initialize SPI channel (channel, baud rate set to 20MHz)
-    spi_init(SPI_PORT, 20000000) ;
-    // Format (channel, data bits per transfer, polarity, phase, order)
-    spi_set_format(SPI_PORT, 16, 0, 0, 0);
+  // Initialize SPI channel (channel, baud rate set to 20MHz)
+  spi_init(SPI_PORT, 20000000) ;
+  // Format (channel, data bits per transfer, polarity, phase, order)
+  spi_set_format(SPI_PORT, 16, 0, 0, 0);
 
-    // Setup the ISR-timing GPIO
-    gpio_init(ISR_GPIO) ;
-    gpio_set_dir(ISR_GPIO, GPIO_OUT);
-    gpio_put(ISR_GPIO, 0) ;
+  // Setup the ISR-timing GPIO
+  gpio_init(ISR_GPIO) ;
+  gpio_set_dir(ISR_GPIO, GPIO_OUT);
+  gpio_put(ISR_GPIO, 0) ;
 
-    // Map SPI signals to GPIO ports
-    gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
-    gpio_set_function(PIN_SCK, GPIO_FUNC_SPI);
-    gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
-    gpio_set_function(PIN_CS, GPIO_FUNC_SPI) ;
+  // Map SPI signals to GPIO ports
+  gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
+  gpio_set_function(PIN_SCK, GPIO_FUNC_SPI);
+  gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
+  gpio_set_function(PIN_CS, GPIO_FUNC_SPI) ;
 
-    // === build the sine lookup table =======
-   	// scaled to produce values between 0 and 4096
-    int ii;
-    for (ii = 0; ii < sine_table_size; ii++){
-         sin_table[ii] = (int)(2047*sin((float)ii*6.283/(float)sine_table_size));
-    }
+  // === build the sine lookup table =======
+  // scaled to produce values between 0 and 4096
+  int ii;
+  for (ii = 0; ii < sine_table_size; ii++){
+        sin_table[ii] = (int)(2047*sin((float)ii*6.283/(float)sine_table_size));
+  }
 
-        // Enable the interrupt for the alarm (we're using Alarm 0)
-    hw_set_bits(&timer_hw->inte, 1u << ALARM_NUM) ;
-    // Associate an interrupt handler with the ALARM_IRQ
-    irq_set_exclusive_handler(ALARM_IRQ, alarm_irq) ;
-    // Enable the alarm interrupt
-    irq_set_enabled(ALARM_IRQ, true) ;
-    // Write the lower 32 bits of the target time to the alarm register, arming it.
-    timer_hw->alarm[ALARM_NUM] = timer_hw->timerawl + DELAY ;
+      // Enable the interrupt for the alarm (we're using Alarm 0)
+  hw_set_bits(&timer_hw->inte, 1u << ALARM_NUM) ;
+  // Associate an interrupt handler with the ALARM_IRQ
+  irq_set_exclusive_handler(ALARM_IRQ, alarm_irq) ;
+  // Enable the alarm interrupt
+  irq_set_enabled(ALARM_IRQ, true) ;
+  // Write the lower 32 bits of the target time to the alarm register, arming it.
+  timer_hw->alarm[ALARM_NUM] = timer_hw->timerawl + DELAY ;
 
   // set up LED gpio 25
   gpio_init(LED_PIN) ;  
   gpio_set_dir(LED_PIN, GPIO_OUT) ;
   gpio_put(LED_PIN, true);
+
+    ////////////////// KEYPAD INITS ///////////////////////
+  // Initialize the keypad GPIO's
+  gpio_init_mask((0x7F << BASE_KEYPAD_PIN)) ;
+  gpio_set_dir((BASE_KEYPAD_PIN+4), GPIO_IN);
+  gpio_set_dir((BASE_KEYPAD_PIN+5), GPIO_IN);
+  gpio_set_dir((BASE_KEYPAD_PIN+6), GPIO_IN);
+  // Set row-pins to output
+  gpio_set_dir_out_masked((0xF << BASE_KEYPAD_PIN)) ;
+  // Set all output pins to low
+  gpio_put_masked((0xF << BASE_KEYPAD_PIN), (0xF << BASE_KEYPAD_PIN)) ;
+  // Turn on pulldown resistors for column pins (on by default)
+  gpio_pull_up((BASE_KEYPAD_PIN+4)) ;
+  gpio_pull_up((BASE_KEYPAD_PIN+5)) ;
+  gpio_pull_up((BASE_KEYPAD_PIN+6)) ;
 
   // === config threads ========================
   pt_add_thread(protothread_toggle25);
@@ -181,8 +263,8 @@ int main(){
   // === initalize the scheduler ===============
   pt_schedule_start ;
 
-    // Nothing happening here
-    while(1){
-    }
-    return 0;
+  // Nothing happening here
+  while(1){
+  }
+  return 0;
 } // end main
