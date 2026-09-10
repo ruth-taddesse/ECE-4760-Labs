e33e894af412dd9203183da8c57f677899f31b6e
Kaelem Bent <kab472@cornell.edu>
2026-09-09 21:19:48 -0400

    Add Lab 1 source files

---
+++ Lab_1/Lab_1_code/ADC_DDS_keypad.c
@@ -0,0 +1,188 @@
+
+/*
+Simple ADC/Protothreads demo
+
+Schedules a single thread, reads/prints ADC value
+
+ */
+
+#include "hardware/gpio.h"
+#include "hardware/timer.h"
+#include "hardware/adc.h"
+#include "pico/stdlib.h"
+#include <stdio.h>
+#include <string.h>
+#include "stdlib.h"
+#include "hardware/spi.h"
+#include <math.h>
+#include "hardware/irq.h"
+
+// ==========================================
+// === protothreads globals
+// ==========================================
+// protothreads header
+#include "pt_cornell_rp2040_v1_4.h"
+
+#define LED_PIN 25
+#define ADC_PIN 26
+#define ADC_MUX 0
+
+// Low-level alarm infrastructure we'll be using
+#define ALARM_NUM 0
+#define ALARM_IRQ timer_hardware_alarm_get_irq_num(timer_hw, ALARM_NUM)
+
+//DDS parameters
+#define two32 4294967296.0 // 2^32 
+#define Fs 50000
+#define DELAY 20 // 1/Fs (in microseconds)
+// the DDS units:
+volatile unsigned int phase_accum_main;
+volatile unsigned int phase_incr_main;
+
+// SPI data
+uint16_t DAC_data ; // output value
+
+//DAC parameters
+// A-channel, 1x, active
+#define DAC_config_chan_A 0b0011000000000000
+// B-channel, 1x, active
+#define DAC_config_chan_B 0b1011000000000000
+
+//SPI configurations
+#define PIN_MISO 4
+#define PIN_CS   5
+#define PIN_SCK  6
+#define PIN_MOSI 7
+#define SPI_PORT spi0
+
+//GPIO for timing the ISR
+#define ISR_GPIO 2
+
+// DDS sine table
+#define sine_table_size 256
+volatile int sin_table[sine_table_size] ;
+
+volatile static unsigned int adc_val ;
+
+static int map(int x, int in_min, int in_max, int out_min, int out_max) {
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+// ==================================================
+// === toggle25 thread 
+// ==================================================
+//  
+static PT_THREAD (protothread_toggle25(struct pt *pt))
+{
+    PT_BEGIN(pt);
+
+      while(1) {
+        // toggle gpio 25
+        gpio_put(LED_PIN, !gpio_get(LED_PIN));
+
+        // Read the ADC
+        adc_val = adc_read() ;
+
+        phase_incr_main = (map(adc_val, 0, 4095, 0, 10000)*two32)/Fs;
+
+        // Print the value
+        printf("ADC value: %d\n", adc_val) ;
+
+        // Yield
+        PT_YIELD_usec(10000) ; //yeild every 10 ms
+      } // END WHILE(1)
+      // every thread ends with PT_END(pt);
+      PT_END(pt);
+} // end blink thread
+
+// Alarm ISR
+static void alarm_irq(void) {
+
+    // Assert a GPIO when we enter the interrupt
+    gpio_put(ISR_GPIO, 1) ;
+
+    // Clear the alarm irq
+    hw_clear_bits(&timer_hw->intr, 1u << ALARM_NUM);
+
+    // Reset the alarm register
+    timer_hw->alarm[ALARM_NUM] = timer_hw->timerawl + DELAY ; 
+
+    // DDS phase and sine table lookup
+    phase_accum_main += phase_incr_main  ;
+    DAC_data = (DAC_config_chan_A | ((sin_table[phase_accum_main>>24] + 2048) & 0xffff))  ;
+
+    // Perform an SPI transaction
+    spi_write16_blocking(SPI_PORT, &DAC_data, 1) ;
+
+    // De-assert the GPIO when we leave the interrupt
+    gpio_put(ISR_GPIO, 0) ;
+
+}
+
+// ========================================
+// === core 0 main
+// ========================================
+int main(){
+  //===  start the serial i/o ==================
+  stdio_init_all() ;
+  // announce the threader version on system reset
+  // if there is a seral terminal attached
+  printf("\n\rProtothreads RP2040 v1.4\n\r");
+
+  // Setup the ADC
+  adc_init() ;
+  adc_gpio_init(ADC_PIN) ;
+  adc_select_input(ADC_MUX) ;
+
+  // Initialize stdio
+    stdio_init_all();
+    printf("Hello, DAC!\n");
+
+    // Initialize SPI channel (channel, baud rate set to 20MHz)
+    spi_init(SPI_PORT, 20000000) ;
+    // Format (channel, data bits per transfer, polarity, phase, order)
+    spi_set_format(SPI_PORT, 16, 0, 0, 0);
+
+    // Setup the ISR-timing GPIO
+    gpio_init(ISR_GPIO) ;
+    gpio_set_dir(ISR_GPIO, GPIO_OUT);
+    gpio_put(ISR_GPIO, 0) ;
+
+    // Map SPI signals to GPIO ports
+    gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
+    gpio_set_function(PIN_SCK, GPIO_FUNC_SPI);
+    gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
+    gpio_set_function(PIN_CS, GPIO_FUNC_SPI) ;
+
+    // === build the sine lookup table =======
+   	// scaled to produce values between 0 and 4096
+    int ii;
+    for (ii = 0; ii < sine_table_size; ii++){
+         sin_table[ii] = (int)(2047*sin((float)ii*6.283/(float)sine_table_size));
+    }
+
+        // Enable the interrupt for the alarm (we're using Alarm 0)
+    hw_set_bits(&timer_hw->inte, 1u << ALARM_NUM) ;
+    // Associate an interrupt handler with the ALARM_IRQ
+    irq_set_exclusive_handler(ALARM_IRQ, alarm_irq) ;
+    // Enable the alarm interrupt
+    irq_set_enabled(ALARM_IRQ, true) ;
+    // Write the lower 32 bits of the target time to the alarm register, arming it.
+    timer_hw->alarm[ALARM_NUM] = timer_hw->timerawl + DELAY ;
+
+  // set up LED gpio 25
+  gpio_init(LED_PIN) ;  
+  gpio_set_dir(LED_PIN, GPIO_OUT) ;
+  gpio_put(LED_PIN, true);
+
+  // === config threads ========================
+  pt_add_thread(protothread_toggle25);
+  
+  // === initalize the scheduler ===============
+  pt_schedule_start ;
+
+    // Nothing happening here
+    while(1){
+    }
+    return 0;
+} // end main
